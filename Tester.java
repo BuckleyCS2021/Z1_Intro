97d27f7e8df8c10200d658807aa8d191654aaf63
sgtpjbarker <89168857+sgtpjbarker@users.noreply.github.com>
2021-10-08 08:32:31 -0700

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,19 @@
+import javax.sound.sampled.Clip;
+
+public class Tester {
+static Thread th;
+
+
+public static void main(String[] args) throws Exception {
+     Clip clip = AudioSystem.getClip();
+     AudioInputStream ais = AudioSystem.getAudioInputStream(new File("/Users/pbarker/eclipse-workspace/test.wav"));
+     clip.open(ais);
+     clip.loop(0);
+     // Calculate totalFrames
+     long totalFrames = (long)(clip.getFrameLength() * clip.getFormat().getFrameRate());
+     Thread.sleep( ( totalFrames* 1000 )); // pause the thread till the sound plays
+
+     System.out.println(clip.getFrameLength());
+     System.out.println(clip.getFormat().getFrameRate());           
+ }
+}
